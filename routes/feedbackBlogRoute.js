334bde9931735c837752fd95560a9c7dd371c46f
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2023-04-04 17:58:42 +0700

    edit API

--- routes/feedbackBlogRoute.js
+++ routes/feedbackBlogRoute.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const {
     createFeedbackBlog,
+    getAllFeedbackProducts,
+    getAFeedbackBlog,
+    deleteFeedbackBlog,
 } = require('../controller/feedbackBlogCtrl')
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post('/', authMiddleware,isAdmin, createFeedbackBlog);
+router.post('/', createFeedbackBlog);
+router.delete('/:id', authMiddleware,isAdmin, deleteFeedbackBlog);
+router.get('/', getAllFeedbackProducts);
+router.get('/:id', getAFeedbackBlog);
+
 module.exports = router;
