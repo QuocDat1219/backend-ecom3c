f55b21d21883bd00a72916d9d1e33060f00bfe89
QuocDung21 <rongvang2357hn@gmail.com>
2023-04-06 10:41:13 +0700

    fix cors

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("api/test", (req, res, next) => {
+app.get("api/test", (req, res, next) => {
   res.json("Da update! ");
 });
 app.use("/api/user", authRouter);
