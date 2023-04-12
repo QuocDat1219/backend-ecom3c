5b6822b6110c71f624963e06de97b1d13b90e6c5
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2023-04-12 13:13:30 +0700

    newhome

--- models/homeModel.js
+++ models/homeModel.js
@@ -1,54 +1,55 @@
-const mongoose = require('mongoose');
-var homeSchema = new mongoose.Schema({
-    imgheader : {
-        public_id: String,
-        secure_url: String,
+const mongoose = require("mongoose");
+var homeSchema = new mongoose.Schema(
+  {
+    imgheader: {
+      public_id: String,
+      secure_url: String,
     },
-    titleheader : {
-        type: String,
-        required: true,
+    titleheader: {
+      type: String,
+      required: true,
     },
-    titleprodcut1:{
-        type: String,
-        required: true,
+    titleprodcut1: {
+      type: String,
+      required: true,
     },
     titleprodcut2: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    imgservice : {
-        public_id: String,
-        secure_url: String,
+    imgservice: {
+      public_id: String,
+      secure_url: String,
     },
-    titleservice : {
-        type: String,
-        required: true,
+    titleservice: {
+      type: String,
+      required: true,
     },
-    descriptionservice : {
-        type: String,
-        required: true,
+    descriptionservice: {
+      type: String,
+      required: true,
     },
-    titlesanpham1 :{
-        type: String,
-        required: true,
+    titlesanpham1: {
+      type: String,
+      required: true,
     },
-    descriptionsanpham1 :{
-        type: String,
-        required: true,
+    descriptionsanpham1: {
+      type: String,
+      required: true,
     },
-    titlesanpham2 :{
-        type: String,
-        required: true,
+    titlesanpham2: {
+      type: String,
+      required: true,
     },
-    descriptionsanpham2 :{
-        type: String,
-        required: true,
-    }
-},
-{
+    descriptionsanpham2: {
+      type: String,
+      required: true,
+    },
+  },
+  {
     timestamps: {
-      currentTime: () => new Date(Date.now() + 7 * 60 * 60 * 1000)
-    }
+      currentTime: () => new Date(Date.now() + 7 * 60 * 60 * 1000),
+    },
   }
 );
-module.exports = mongoose.model('footerinfo',homeSchema);
+module.exports = mongoose.model("footerinfo", homeSchema);
