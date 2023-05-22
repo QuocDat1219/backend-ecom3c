2986f2da1977fcade0e9d888364732d458a9d725
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2023-05-22 21:49:07 +0700

    newwwwwww

--- models/feedbackBlogModel.js
+++ models/feedbackBlogModel.js
@@ -1,30 +1,27 @@
-const mongoose = require('mongoose');
-var feedbackBlog = new mongoose.Schema({
-    email:{
-        type: String,
-        required: true,
+const mongoose = require("mongoose");
+var feedbackBlog = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
     },
-    comment:{
-        type: String,
-        required: true,
+    comment: {
+      type: String,
+      required: true,
     },
-    usename:{
-        type: String,
-        required: true,
+    usename: {
+      type: String,
+      required: true,
     },
-    website:{
-        type: String,
-        required: true,
+    idblog: {
+      type: String,
+      required: true,
     },
-    idblog:{
-        type: String,
-        required: true,
-    }
-},
-{
+  },
+  {
     timestamps: {
-      currentTime: () => new Date(Date.now() + 7 * 60 * 60 * 1000)
-    }
+      currentTime: () => new Date(Date.now() + 7 * 60 * 60 * 1000),
+    },
   }
 );
-module.exports = mongoose.model('feedbackBlog', feedbackBlog)
+module.exports = mongoose.model("feedbackBlog", feedbackBlog);
